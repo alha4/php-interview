46649b1c64ca13a7c958dc2aa56891f30076a451
igor teh <a.i.n.1989@mail.ru>
2024-07-01 18:11:07 +0200

    [fix] - введены интерфейсы компонентов и обработчиков

--- architecture/solid_o.php
+++ architecture/solid_o.php
@@ -3,27 +3,19 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 use App\Components\{SomeObjectSqS, SomeObjectNats};
+use App\Handlers\IterableObjectsHandler;
+use App\Handlers\QueueObjectHandler;
 
-class SomeObjectsHandler
-{
-
-    public function handleObjects(array $objects): array
-    {
-        $handlers = [];
-
-        foreach ($objects as $object) {
-
-            $handlers[] = $object->handle();
-        }
-
-        return $handlers;
-    }
-}
-
+/**
+ * метод handleObjects закрыт от модицикации и открыт для расширения
+ */
 $objects = [
     new SomeObjectSqS('object_sqs'),
     new SomeObjectNats('object_nats'),
 ];
 
-$soh = new SomeObjectsHandler();
-$soh->handleObjects($objects);
+$iterable = new IterableObjectsHandler();
+$iterable->handleObjects($objects);
+
+$queue = new QueueObjecthandler();
+$queue->handleObjects($objects);
